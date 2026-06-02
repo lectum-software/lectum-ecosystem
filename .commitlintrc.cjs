06174b7c913918b3d42c6dce2fb79e73d28e9bec
Igor Rezende <sr.igor.dev@gmail.com>
2026-06-02 18:44:24 -0300

    feat: initial commit

---
+++ .commitlintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'pack',
+        'adr',
+      ],
+    ],
+    'subject-case': [2, 'never', ['upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 80],
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 120],
+    'footer-leading-blank': [2, 'always'],
+  },
+};
